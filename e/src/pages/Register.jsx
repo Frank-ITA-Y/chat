b5505fd28044a20a16eb08f5c42d0b532410f961
Frank-ITA-Y <111883676+Frank-ITA-Y@users.noreply.github.com>
2023-02-08 14:36:22 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- e/src/pages/Register.jsx
+++ e/src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Add from "../img/addAvatar.png"
 
 const Register = () => {
     return (
@@ -10,7 +11,11 @@ const Register = () => {
                     <input type="text" placeholder="diplay name"/>
                     <input type="email" placeholder="email"/>
                     <input type="password" placeholder="password"/>
-                    <input type="file" />
+                    <input style={{display:"none"}} type="file" id='file'/>
+                    <label htmlFor="file">
+                        <img src={Add} alt="" />
+                        <span>Add an avatar</span>
+                    </label>
                     <button>Sing up</button>
                 </form>
                 <p>You do have account? Login</p>
