cac3313ba57094d7a6fbc494677529ee02951566
Frank-ITA-Y <111883676+Frank-ITA-Y@users.noreply.github.com>
2023-02-09 19:54:15 +0000

    dcfygtuo

--- e/src/components/Sidebar.jsx
+++ e/src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Search from '../components/Search'
+import Chats from '../components/Chats'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <Navbar />
         <Search />
+        <Chats />
     </div>
   )
 }
