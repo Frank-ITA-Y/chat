a4becf6b4e1326392dc03b78c8abb37df4f4c829
Frank-ITA-Y <111883676+Frank-ITA-Y@users.noreply.github.com>
2023-02-12 12:39:46 +0000

    fffffffffffffffffffffrrrrrrrrrrrrrr

--- e/src/components/Message.jsx
+++ e/src/components/Message.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message'>
+      <div className='messageInfo'>
+        <img src="https://images.pexels.com/photos/15115627/pexels-photo-15115627.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://images.pexels.com/photos/15115627/pexels-photo-15115627.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        
+      </div>
+    </div>
   )
 }
 
